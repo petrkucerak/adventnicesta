091f4f57888c9ab79548b83d995d267bda7db7e5
Petr Kucera <petr@khome.cz>
2022-11-25 11:04:48 +0100

    Add style

--- components/utils/date.js
+++ components/utils/date.js
@@ -3,10 +3,10 @@
  * @param {*} dateString format YYYY-MM-DD (example: 2022-12-01)
  * @returns DD. MM. YYYYY
  */
-export default function RenderDate({ dateString }) {
+export default function RenderDate({ dateString, className}) {
   const date = new Date(dateString);
   const out = `${date.getDate()}. ${
     date.getMonth() + 1
   }. ${date.getFullYear()}`;
-  return <span>{out}</span>;
+  return <span className={className}>{out}</span>;
 }
