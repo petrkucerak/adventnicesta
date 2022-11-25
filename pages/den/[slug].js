46f900a3e27e46ab60a4fbc12532948c197906ab
Petr Kucera <petr@khome.cz>
2022-11-25 11:31:01 +0100

    Insert a text

--- pages/den/[slug].js
+++ pages/den/[slug].js
@@ -2,15 +2,33 @@ import Error from "next/error";
 import { useRouter } from "next/router";
 import Layout from "../../components/layout";
 import { getPostBySlug, getAllPosts } from "../../lib/api";
+import Head from "next/head";
+import RenderDate from "../../components/utils/date";
+import ArticleHeader from "../../components/header";
 
 export default function Post({ post, preview }) {
   const router = useRouter();
   if (!router.isFallback && !post?.slug) return <Error statusCode={404} />;
-
+  const title = `${post.dayName} - Adventní cesta`;
   return (
-    <Layout>
-      <h1>{post.dayName}</h1>
-    </Layout>
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Layout>
+        <ArticleHeader />
+        <article className="mx-4 max-w-[800px]">
+          <h1 className="text-4xl font-bold mb-4">{post.dayName}</h1>
+          <RenderDate dateString={post.date} className="font-semibold" />
+          <h2 className="text-xl font-bold mt-4 mb-2">Úryvek z Bible</h2>
+          <p className="">{post.quote}</p>
+          <h2 className="text-xl font-bold mt-4 mb-2">Zamyšlení</h2>
+          <p>{post.reflexion}</p>
+          <h2 className="text-xl font-bold mt-4 mb-2">Vstupní modlitba</h2>
+          <p>{post.preayer}</p>
+        </article>
+      </Layout>
+    </>
   );
 }
 
