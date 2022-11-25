7f8329f66585b253f9be499b30733c96d0b11a61
Petr Kucera <petr@khome.cz>
2022-11-26 00:10:17 +0100

    Add slova Anny Maclove

--- pages/slova-Anny-Maclove.js
+++ pages/slova-Anny-Maclove.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import ArticleHeader from "../components/header";
 
 export default function SlovaAnnyMaclove() {
   return (
@@ -7,7 +8,73 @@ export default function SlovaAnnyMaclove() {
       <Head>
         <title>Slova Anny Maclové - Adventní cesta</title>
       </Head>
-      <Layout></Layout>
+      <Layout>
+        <ArticleHeader />
+        <article className="mx-6 max-w-[800px]">
+          <h1 className="text-4xl font-bold mb-8">Slova Anny Maclové</h1>
+          <p className="font-semibold text-lg mb-1">Vážení a milí,</p>
+          <p className="text-lg mb-4">
+            děkuji vám, že držíte v rukou tuto brožuru a necháte se jí vést
+            letošní dobou adventní až do Štědrého dne.
+          </p>
+          <p className="text-lg mb-4">
+            V průběhu Adventu se připravujeme na nejkrásnější svátky v roce –
+            Vánoce. Neznám mnoho lidí, kteří se na ně netěší. Obvykle jsou to
+            lidé vnitřně strádající, osamělí, opuštění. Ale také jim je určena
+            ta radostná zvěst, všichni bez rozdílu jsme zváni do Betléma k
+            jesličkám.
+          </p>
+          <p className="text-lg mb-4">
+            Bůh, který je LÁSKA, nám sesílá na svět svého Syna v podobě malého
+            Dítěte a nabízí nám příležitost přijmout lásku do svého srdce, učit
+            se lásce a v ní růst, lásku zakoušet a ji sdílet. Na naší životní
+            cestě nás nenechává samotné, ale jako laskavý Otec se stará a nabízí
+            nám přítomnost svého Syna Ježíše Krista, jehož následováním smíme
+            zakoušet smysl své existence, vnímat naději a radost z toho, že nás
+            má nekonečně rád a v závěru spočinout v Jeho láskyplné náruči.
+          </p>
+          <p className="text-lg mb-4">
+            Kdo otevře své srdce a přijme tuto Boží lásku, má v úctě sebe i
+            bližního svého, má potřebu lásku sdílet a rozdávat v drobných
+            skutcích každý den, vidí kolem sebe nejenom své blízké, ale vnímá
+            také ty, kteří nikoho nemají, jsou bezradní, žijí na ulici či
+            zavřeni ve svém bytě bez kontaktu s okolím, jsou nemocní, potřebují
+            pomoc. Boží láska proměňuje také celou komunitu. Církev usiluje o
+            šíření zvěsti o Bohu skrze Slovo a svátosti, ale také skrze službu
+            zejména lidem trpícím, ohroženým a žijícím na okraji společnosti.
+          </p>
+          <p className="text-lg mb-4">
+            Církev uplatňuje toto své poslání již od svého zrodu skrze různé
+            struktury. V tomto období si připomínáme století organizovaného
+            charitního díla na území naší republiky a 30. výročí založení
+            Diecézní katolické charity Hradec Králové a některých farních či
+            oblastní charit v naší diecézi, které navázaly na rozmach Charity
+            zejména v meziválečném období, která byla později v dobách komunismu
+            spíš trpěna a značně ochromena.
+          </p>
+          <p className="text-lg mb-4">
+            Právě při této příležitosti jsme oslovili ty, kteří v Charitě v naší
+            diecézi pracují jako zaměstnanci nebo dobrovolníci, případně nás
+            duchovně provázejí. Přijměte proto úvahy těch, kteří zareagovali a
+            nabídli své reflexe na zadané čtení.
+          </p>
+          <p className="text-lg mb-4">
+            Zvu vás tedy na cestu k Jesličkám spolu s těmi, kteří se různým
+            způsobem podílejí na šíření zvěsti o tom, že BŮH JE LÁSKA
+            praktickými skutky – dochází k nemocným, radí bezradným, koordinují
+            provoz organizace či řídí charitní zařízení, dělí se o svůj volný
+            čas s potřebnými.
+          </p>
+          <p className="text-lg mb-4">
+            Pojďme spolu do Betléma, přivítejme Božího Syna – malé Jezulátko a
+            po Novém roce se vydejme spolu s králi přát všem lidem štěstí,
+            zdraví, pokoj, usilujme o to, aby vánoční mysterium lásky odezněním
+            svátků nevyprchalo, ale žili jsme ho všichni dál a každý den tam,
+            kde zrovna jsme.
+          </p>
+          <p className="text-lg mb-8 italic">Anna Maclová</p>
+        </article>
+      </Layout>
     </>
   );
 }
--- pages/slova-biskupa-Jana.js
+++ pages/slova-biskupa-Jana.js
@@ -8,46 +8,51 @@ export default function SlovaBiskupaJana() {
       <Head>
         <title>Slova Biskupa Jana - Adventní cesta</title>
       </Head>
-      <ArticleHeader />
-      <article className="mx-6 max-w-[800px]">
-        <h1 className="text-4xl font-bold mb-8">Slova biskupa Jana</h1>
-        <p className="font-semibold text-lg mb-1">Bratři a sestry v Kristu, milí čtenáři,</p>
-        <p className="text-lg mb-4">
-          adventní doba je příliš krátká na to, abychom si v ní plně uvědomili
-          velikost Božího daru, za který Mu o Vánocích děkujeme: náš Stvořitel s
-          námi přichází do přímého, mezilidského kontaktu. Nabízí nám osobní
-          vztah, který navíc přesahuje časnost tohoto světa. Vlastně ani celý
-          život není dost dlouhý na pochopení tohoto tajemství. Když však Ježíš
-          vyzývá učedníky, aby Ho následovali, nevybízí je jenom k abstraktním
-          úvahám. Především chce, aby vstali, vyšli mezi druhé lidi a
-          napodobovali Ho. Aby činili to, co činil a činí On.
-        </p>
-        <p className="text-lg mb-4">
-          Ač mnohá Kristova slova, činy a podobenství nechávají prostor
-          přemýšlení o jejich významu, v základu všech je beze sporu
-          milosrdenství. Bůh člověku prokazuje nezaslouženou milost. Přichází za
-          ním, i když nemusí, učí ho a vede, i když nemusí, obětuje se za něho,
-          i když nemusí. Napodobovat našeho Pána tedy znamená v první řadě činit
-          skutky nezištné lásky. Je k nim povolán každý křesťan jako jednotlivec
-          i Církev jako společenství na místní, diecézní i celosvětové úrovni.
-          Charita, jak se tento její rozměr tradičně nazývá, tedy není jen
-          doplňkovou institucí, ale projevem samotného základu našeho
-          křesťanského povolání. Není jen záležitostí hromadných sbírek či
-          finančních darů, ale výchozího životního postoje: každý z nás je do
-          života povolán především proto, aby konkrétními skutky prokazoval
-          druhým caritas, nezištnou, milosrdnou lásku.
-        </p>
-        <p className="text-lg mb-4">
-          Pokud se v adventní době vydáme právě touto cestou, pak se pravé
-          podstatě vánočního daru přiblížíme mnohem více než sebevětší snahou
-          vyložit jej teoreticky, „od stolu“. Děkuji autorům všech textů v této
-          brožuře, jejichž moudrost a zkušenosti nás budou letošní adventní
-          dobou v tomto smyslu provázet, a ze srdce Vám na jejím počátku žehnám!
-        </p>
-        <p className="text-lg mb-1 italic">V Kristu a Marii,</p>
-        <p className="text-lg mb-8 italic">Váš + biskup Jan</p>
-      </article>
-      <Layout></Layout>
+      <Layout>
+        <ArticleHeader />
+        <article className="mx-6 max-w-[800px]">
+          <h1 className="text-4xl font-bold mb-8">Slova biskupa Jana</h1>
+          <p className="font-semibold text-lg mb-1">
+            Bratři a sestry v Kristu, milí čtenáři,
+          </p>
+          <p className="text-lg mb-4">
+            adventní doba je příliš krátká na to, abychom si v ní plně uvědomili
+            velikost Božího daru, za který Mu o Vánocích děkujeme: náš Stvořitel
+            s námi přichází do přímého, mezilidského kontaktu. Nabízí nám osobní
+            vztah, který navíc přesahuje časnost tohoto světa. Vlastně ani celý
+            život není dost dlouhý na pochopení tohoto tajemství. Když však
+            Ježíš vyzývá učedníky, aby Ho následovali, nevybízí je jenom k
+            abstraktním úvahám. Především chce, aby vstali, vyšli mezi druhé
+            lidi a napodobovali Ho. Aby činili to, co činil a činí On.
+          </p>
+          <p className="text-lg mb-4">
+            Ač mnohá Kristova slova, činy a podobenství nechávají prostor
+            přemýšlení o jejich významu, v základu všech je beze sporu
+            milosrdenství. Bůh člověku prokazuje nezaslouženou milost. Přichází
+            za ním, i když nemusí, učí ho a vede, i když nemusí, obětuje se za
+            něho, i když nemusí. Napodobovat našeho Pána tedy znamená v první
+            řadě činit skutky nezištné lásky. Je k nim povolán každý křesťan
+            jako jednotlivec i Církev jako společenství na místní, diecézní i
+            celosvětové úrovni. Charita, jak se tento její rozměr tradičně
+            nazývá, tedy není jen doplňkovou institucí, ale projevem samotného
+            základu našeho křesťanského povolání. Není jen záležitostí
+            hromadných sbírek či finančních darů, ale výchozího životního
+            postoje: každý z nás je do života povolán především proto, aby
+            konkrétními skutky prokazoval druhým caritas, nezištnou, milosrdnou
+            lásku.
+          </p>
+          <p className="text-lg mb-4">
+            Pokud se v adventní době vydáme právě touto cestou, pak se pravé
+            podstatě vánočního daru přiblížíme mnohem více než sebevětší snahou
+            vyložit jej teoreticky, „od stolu“. Děkuji autorům všech textů v
+            této brožuře, jejichž moudrost a zkušenosti nás budou letošní
+            adventní dobou v tomto smyslu provázet, a ze srdce Vám na jejím
+            počátku žehnám!
+          </p>
+          <p className="text-lg mb-1 italic">V Kristu a Marii,</p>
+          <p className="text-lg mb-8 italic">Váš + biskup Jan</p>
+        </article>
+      </Layout>
     </>
   );
 }
