8de525c5ff5bbb2f51328d4c19e919bf62d0b41c
Petr Kucera <petr@khome.cz>
2022-11-25 10:48:49 +0100

    Footer style

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div>
-      <p>Hi, I&apos;m the footer</p>
-    </div>
+    <footer className="bg-gray-200 py-4">
+      <p className="text-center">
+        Chceš o projektu vědět víc, mrkni do{" "}
+        <Link
+          href="https://github.com/petrkucerak/adventnicesta"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="underline"
+        >
+          repa
+        </Link>
+        .
+      </p>
+    </footer>
   );
 }
