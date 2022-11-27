0f5a62f258a69e4ccbbb7e7840b72861194bf7d5
petrkucerak <petr@khome.cz>
2022-11-27 21:00:57 +0100

    Correct config of last next-pwa version

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// const withPWA = require("next-pwa")({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development",
+//   },
+// });
+
+// module.exports = withPWA();
+
 const withPWA = require("next-pwa")({
   dest: "public",
 });
