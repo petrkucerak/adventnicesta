0c6ceb941decb6c50da572923ab146ff9eaca468
petrkucerak <petr@khome.cz>
2022-11-27 21:05:44 +0100

    Set up register and skipWaiting in next-pwa

--- next.config.js
+++ next.config.js
@@ -17,4 +17,6 @@ const withPWA = require("next-pwa")({
 module.exports = withPWA({
   // next.js config
   disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
