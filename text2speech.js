b3264a21378eed2c6483900e4095ebeb4edbb95a
petrkucerak <petr@khome.cz>
2022-11-28 09:54:32 +0100

    Print info

--- text2speech.js
+++ text2speech.js
@@ -2,11 +2,12 @@ const sdk = require("microsoft-cognitiveservices-speech-sdk");
 const fs = require("fs");
 
 module.exports.text2speech = () => {
+    console.log("~~~~~~~~ TEXT TO SPEECH ~~~~~~~~");
   // process variables
   const argv = process.argv;
   if (argv[1] === "" || argv[2] === "") {
     console.error("ERROR: Set up SPEECH_KEY and LOCATION_REGION!");
-    return;
+    return 1;
   }
   allJsons(argv[1], argv[2]);
 
