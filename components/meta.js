d4e76bdb8fff8aa9fe076e94786e657eff392c75
petrkucerak <petr@khome.cz>
2022-11-28 13:14:16 +0100

    Improve accessibility

--- components/meta.js
+++ components/meta.js
@@ -41,7 +41,7 @@ export default function Meta() {
       <meta name="theme-color" content={mainColor} />
       <meta
         name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
       />
       <meta name="application-name" content={appName} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
