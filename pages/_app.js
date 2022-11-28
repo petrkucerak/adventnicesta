a8f6c38f3c1a0362e324cf3d283f018b3c28924d
petrkucerak <petr@khome.cz>
2022-11-28 09:03:11 +0100

    Add OneSignal SDK

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
 import "../styles/dist.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.register(
+        `/OneSignalSDKWorker.js`
+      ).then(/*reg => console.log(reg)*/);
+    }
+  }, []);
+
+  // notifications
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "575b275b-b7c2-48db-951e-c26ae2cc4a0c",
+        notifyButton: {
+          enable: true,
+        },
+
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []); // <-- run this effect once on mount
   return <Component {...pageProps} />;
 }
 
