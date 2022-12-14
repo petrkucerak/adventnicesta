ee006651678eed9e300788fefae67f39aad942e5
petrkucerak <petr@khome.cz>
2022-12-14 08:21:37 +0100

    Add optional parametrs

--- components/utils/button.js
+++ components/utils/button.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
-export default function Button({ href, content }) {
+export default function Button({ href, content, rel, target }) {
   return (
     <Link
       href={href}
       className="bg-violet-light rounded text-white px-4 py-2 font-semibold hover:scale-110 duration-200 my-4"
+      rel={rel}
+      target={target}
     >
       <button title={content}>{content}</button>
     </Link>
