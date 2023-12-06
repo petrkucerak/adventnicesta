c23aead65af825558ea4f55c64682eff70266051
petrkucerak <petr@khome.cz>
2023-12-06 23:37:14 +0100

    Remove OneSignalSDK

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require("next-pwa")({
 module.exports = withPWA({
   // next.js config
   disable: process.env.NODE_ENV === "development",
-  register: false,
+  register: true,
   skipWaiting: true,
   cacheOnFrontEndNav: true,
   publicExcludes: ["!favicon/*/**", "!audio/**"],
