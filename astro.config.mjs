0cbf7bffb90c65b43d2285e6566d667caff30401
petrkucerak <petr@khome.cz>
2024-11-27 08:43:42 +0100

    Remove vite-pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import AstroPWA from "@vite-pwa/astro";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://adventnicesta.cz/",
-  integrations: [
-    tailwind(),
-    sitemap(),
-    AstroPWA({
-      workbox: {
-        navigateFallback: "/",
-        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
-      },
-      devOptions: {
-        enabled: true,
-        navigateFallbackAllowlist: [/^\//],
-      },
-      experimental: {
-        directoryAndTrailingSlashHandler: true,
-      },
-    }),
-  ],
+  integrations: [tailwind(), sitemap()],
 });
