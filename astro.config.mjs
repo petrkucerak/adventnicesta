8df51bc8d05fe36f8585c68cccfd077b3e263c6a
petrkucerak <petr@khome.cz>
2024-11-27 08:35:45 +0100

    Install vite PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,27 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
+import AstroPWA from "@vite-pwa/astro";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://adventnicesta.cz/",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    AstroPWA({
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
 });
